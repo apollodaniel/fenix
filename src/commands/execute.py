d2090e21b8f506b10ef93db330a06d90750f1e9b
farofaDeCachorro <87376389+farofaDeCachorro@users.noreply.github.com>
2021-07-27 19:15:07 -0300

    Create execute.py

---
+++ src/commands/execute.py
@@ -0,0 +1,14 @@
+from os import listdir, system
+from src.util import echoErrorMessage
+def executeCommand(dirs: dict, args: tuple):
+    for d in listdir(dirs['apps']):
+        if d.startswith(args[0]):
+            command = f"{dirs['apps']}/{d}"
+            try:
+                if args[1] is not None:
+                    command += " " + args[1]
+            except:
+                pass
+            system(command)
+            return
+    echoErrorMessage(f"{args[0]} isn't not installed.")
